b096b2daa49422b7c8d9b20d48c6b7b21c168217
Shani Koenigsberg <skoenigs2@student.touro.edu>
2021-11-03 16:08:07 -0400

    basic outline of bet balance class

---
+++ src/BetBalancePackage/BasicBetBalanceCalc.java
@@ -0,0 +1,33 @@
+package BetBalancePackage;
+
+public class BasicBetBalanceCalc {
+
+	private double balance;
+	private double minBalance;
+	private IRandomValueGenerator myRandom;
+	
+
+	public BasicBetBalanceCalc() {
+	}
+
+	public double getCurrentBalance() {
+		return 0;
+	}
+
+	public boolean canBet() {
+		return false;
+	}
+
+	public void addMoney() {
+		
+	}
+
+	public double betOnANumber() {
+		return 0;
+	}
+	
+	public double betOnProbability() {
+		return 0;
+	}
+	
+}
